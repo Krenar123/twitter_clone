9278226e410db5a3ec97e137e510adcce9c57c42
Krenar123 <krenardardhishta205@gmail.com>
2020-05-14 18:23:53 +0200

    Model Yweet is generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
+  get 'tweets/index', to: 'tweets#index'
+
+  root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
