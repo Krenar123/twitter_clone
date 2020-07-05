871a6d870249b9f341e2fe86cecd6dbacd1f270c
Krenar123 <krenardardhishta205@gmail.com>
2020-07-06 01:37:10 +0200

    Model Like created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_223031) do
+ActiveRecord::Schema.define(version: 2020_07_05_233319) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "tweet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "retweets", force: :cascade do |t|
     t.string "rtweet"
@@ -39,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_07_05_223031) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "retweets", "tweets"
   add_foreign_key "retweets", "users"
   add_foreign_key "tweets", "users"
