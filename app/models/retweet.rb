e7d2ccea7a715fc4de8d9023239558291ca3b019
Krenar123 <krenardardhishta205@gmail.com>
2020-07-07 15:01:57 +0200

    Testing model retweet with rspec

--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -1,5 +1,7 @@
 class Retweet < ApplicationRecord
+  MIN_RTWEET_LENGTH = 10
+  MAX_RTWEET_LENGTH = 100
   belongs_to :tweet
   belongs_to :user
-  validates :rtweet, presence: true
+  validates :rtweet, presence: true, length: {in: (MIN_RTWEET_LENGTH)..(MAX_RTWEET_LENGTH)}
 end
--- spec/models/retweet_spec.rb
+++ spec/models/retweet_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Retweet do
+    describe 'validations' do
+        context 'presence' do
+          it { should validate_presence_of(:rtweet)}
+        end
+    
+        context 'length' do
+          it { should validate_length_of(:rtweet).is_at_least(Retweet::MIN_RTWEET_LENGTH).is_at_most(Retweet::MAX_RTWEET_LENGTH)}
+        end
+    end
+    
+    describe 'authentications' do
+        it { should belong_to(:tweet) }
+        it { should belong_to(:user) }
+    end
 end
