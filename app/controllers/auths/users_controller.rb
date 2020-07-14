e7e6ce10fa17e3b05f2f3b66036a8d9eb1d05f89
Krenar123 <krenardardhishta205@gmail.com>
2020-07-14 11:49:08 +0200

    Options to edit profile

--- app/controllers/auths/users_controller.rb
+++ app/controllers/auths/users_controller.rb
@@ -7,6 +7,21 @@ class Auths::UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def edit
+        @user = User.find(params[:id])
+        session_alert('alert','You dont have permission to edit!', auths_user_path(@user)) unless logged_in? &&  user_equals?(@user)
+    end
+
+    def update
+        @user = User.find(params[:id])
+        session_alert('alert','You dont have permission to edit!', auths_user_path(@user)) unless logged_in? && user_equals?(@user)
+        if @user.update(user_params)
+            redirect_to auths_user_path(@user)
+        else
+            render :edit 
+        end
+    end
+
     def create 
         @user = User.new(user_params)
 
